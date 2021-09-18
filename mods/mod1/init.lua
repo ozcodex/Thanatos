190af75fadaac563bcf9138fd5b5f02e56f92e09
Oscar Bernal <oscarbernal93@gmail.com>
2021-09-18 15:07:14 -0500

    add test item and animations to a block

--- mods/mod1/init.lua
+++ mods/mod1/init.lua
@@ -1,7 +1,17 @@
 -- Nodes
 minetest.register_node("mod1:test_block", {
 	description = "Test Block",
-	tiles = {"test_block.png"},
+	tiles = {"test_block.png","test_block.png",
+		{
+			name ="test_block_animated.png",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1.5,
+			}
+		}
+	},
 	groups = {oddly_breakable_by_hand = 1}
 })
 
@@ -14,7 +24,19 @@ minetest.register_node("mod1:refined_test_block", {
 minetest.register_node("mod1:test_block_pieces", {
 	description = "Test Block Pieces",
 	tiles = {"test_block_pieces.png"},
+	drop = "mod1:test_fragment 2",
 	groups = {oddly_breakable_by_hand = 2}
+})
+
+-- Items
+
+minetest.register_craftitem("mod1:test_fragment", {
+	description = "Test Block Fragment",
+	inventory_image = "test_fragment.png",
+	on_use = function(itemstack, user, pointed_thing)
+		itemstack:take_item(1)
+		return itemstack
+	end,
 })
 
 -- Crafting
@@ -22,6 +44,21 @@ minetest.register_craft({
 	output = "mod1:test_block 2",
 	recipe = {
 		{"default:dirt","default:dirt"},
+	}
+})
+
+minetest.register_craft({
+	output = "mod1:test_fragment 2",
+	recipe = {
+		{"mod1:test_block"},
+	}
+})
+
+
+minetest.register_craft({
+	output = "mod1:test_block_pieces",
+	recipe = {
+		{"mod1:test_fragment","mod1:test_fragment"},
 	}
 })
 
@@ -34,9 +71,9 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "mod1:test_block_pieces 1",
+	output = "mod1:test_block",
 	recipe = {
-		{"default:stick","mod1:refined_test_block"},
+		{"default:stick","mod1:test_block_pieces"},
 	},
 	replacements = {{"default:stick","default:stick"}}
 })
