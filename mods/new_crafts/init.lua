f62fcedfa90cb8b5e6cc9642b58a8f0ff2733051
Oscar Bernal <oscarbernal93@gmail.com>
2022-02-03 11:41:08 -0500

    Add flint chunk and modify gravel to drop it

--- mods/new_crafts/init.lua
+++ mods/new_crafts/init.lua
@@ -2,17 +2,31 @@ minetest.override_item('default:leaves',{
   drop = {
     max_items = 1,
     items = {
-      {
-        items = {'default:sapling'},
-        rarity = 20, -- 1/20
-      },
-      {
-        items = {'default:stick'},
-        rarity = 5
-      },
-      { 
-        items= {'default:leaves'}
-      }
+      { items = {'default:sapling'}, rarity = 20 },
+      { items = {'default:stick'}, rarity = 8 },
+      { items= {'default:leaves'} }
     }
   }
+})
+
+minetest.override_item('default:gravel',{
+  drop = {
+    max_items = 1,
+    items = {
+      { items = {'default:flint'}, rarity = 16 },
+      { items = {'new_crafts:flint_chunk'}, rarity = 32 },
+      { items= {'default:gravel'} }
+    }
+  }
+})
+
+minetest.register_node("new_crafts:flint_chunk", {
+  description = "A big chunk of flint",
+  drawtype = 'mesh',
+  mesh = 'flint_chunk.obj',
+  tiles = {"flint_chunk.png"},
+  groups = {oddly_breakable_by_hand = 2},
+  paramtype2 = "facedir",
+  selection_box = { type = 'fixed', fixed = {-0.5, -0.5, -0.5, 0.5, 0.0, 0.5} },
+  collision_box = { type = 'fixed', fixed = {-0.5, -0.5, -0.5, 0.5, 0.0, 0.5} }
 })
